154cba52cf7159a05259dc766702c05bcccc3d65
BruceVSOL <hjkl0001@outlook.com>
2019-01-15 17:40:21 +0800

    add test.cs

---
+++ ConsoleApp1/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class test
+    {
+        public static string getStr()
+        {
+            return "Hello World";
+        }
+    }
+}
